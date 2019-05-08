eda8abda3ddc590593dd4486af772a048b238872
Emeka Ugwuanyi <emylincon@gmail.com>
2019-05-08 10:52:46 +0100

    first working progress

---
+++ proposed.py
@@ -0,0 +1,108 @@
+from functools import reduce
+from sys import *
+
+
+def gcd(a, b):
+    if b == 0: return a
+    return gcd(b, a % b)
+
+
+def lcm(a, b):
+    return int(a * b / gcd(a, b))
+
+
+def LCM(list):
+    return reduce(lcm, list)
+
+
+def load_tasks():
+
+    tasks = {'t1': {'wcet': 3, 'period': 20},
+             't2': {'wcet': 2, 'period': 5},
+             't3': {'wcet': 2, 'period': 10}
+             }
+    period_list = [tasks[i]['period'] for i in tasks]
+
+    lcm_period = LCM(period_list)
+    # insert idle task
+    tasks['idle'] = {'wcet': lcm_period, 'period': lcm_period + 1}
+    return tasks, lcm_period
+
+
+def scheduler(tasks, D):
+    queue = list(tasks.keys())  # initialize task queue
+    schedule = []
+    curr = ''  # current task
+    prev = ''  # previous task
+    tmp = {}
+    for task in tasks.keys():
+        tmp[task] = {}  # temporary data for each task
+        tmp[task]['deadline'] = tasks[task]['period']
+        tmp[task]['executed'] = 0
+
+    # start scheduling...
+    # proceed by one timestamp to handle preemption
+    for time in range(D):
+        # insert new tasks into the queue
+        for t in tmp.keys():
+            if time == tmp[t]['deadline']:
+                if tasks[t]['wcet'] > tmp[t]['executed']:
+                    print('Scheduling Failed at %d' % time)
+                    exit(1)
+                else:
+                    tmp[t]['deadline'] += tasks[t]['period']
+                    tmp[t]['executed'] = 0
+                    queue.append(t)
+        # select next task to be scheduled
+        min = D * 2
+        for task in queue:
+            if tmp[task]['deadline'] < min:
+                min = tmp[task]['deadline']
+                curr = task
+        tmp[curr]['executed'] += 1
+        print(time, queue, curr)
+
+        # dequeue the execution-completed task
+        if tmp[curr]['executed'] == tasks[curr]['wcet']:
+            for i in range(len(queue)):
+                if curr == queue[i]:
+                    del queue[i]
+                    break
+
+        # record to the schedule trace
+        if prev != curr:
+            if prev in queue and prev != 'idle':  # previous task is preempted..
+                s = schedule.pop()
+                schedule.append([s[0], s[1], '*'])
+            schedule.append([time, curr])
+        prev = curr
+
+    # check correctness
+    k = {}
+    for i in tasks.keys(): k[i] = 0
+    for s in schedule:
+        if len(s) == 2:  # count unpreempted tasks
+            k[s[1]] += 1
+    print(k)
+    return schedule
+
+
+def print_schedule(s):
+    print(f's:{s}')
+    print(' -----------------------')
+    print('|\ttime\ttask\t|')
+    print(' -----------------------')
+    for i in range(len(s)):
+        t = s.pop(0)
+        if len(t) == 2:
+            print('|\t{}\t{}\t|'.format(t[0], t[1].strip()))
+        else:
+            print('|\t{}\t{}\t|'.format(t[0], t[1].strip() + t[2].strip()))
+    print(' -----------------------')
+    print(s)
+
+
+if __name__ == '__main__':
+    a, b = load_tasks()
+    s = scheduler(a, b)
+    print_schedule(s)
