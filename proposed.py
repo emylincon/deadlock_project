1c2e9856ece9ac55ec0bee026f44436a975a747f
Emeka Ugwuanyi <emylincon@gmail.com>
2019-05-13 22:51:07 +0100

    adding cooperative functionality using multicasting message

--- proposed.py
+++ proposed.py
@@ -3,6 +3,25 @@ from sys import *
 import numpy as np
 import random as r
 import ping_code as pc
+import socket
+import struct
+import subprocess as sp
+from threading import Thread
+import time
+
+hosts = {}  # {hostname: ip}
+multicast_group = '224.3.29.71'
+server_address = ('', 10000)
+
+# Create the socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Bind to the server address
+sock.bind(server_address)
+# Tell the operating system to add the socket to the multicast group
+# on all interfaces.
+group = socket.inet_aton(multicast_group)
+mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 tasks = {'t1': {'wcet': 3, 'period': 20},
          't2': {'wcet': 2, 'period': 5},
