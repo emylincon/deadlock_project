dca90d7af91e6729e1cac1f86b12e1347f52b77a
Emeka Ugwuanyi <emylincon@gmail.com>
2019-05-31 21:12:06 +0100

    done! data structure changed

--- algo/test/2edf.py
+++ algo/test/2edf.py
@@ -2,9 +2,9 @@ from functools import reduce
 
 # tasks = {ti: [capacity, deadline, period]}
 tasks = {
-    't1': [3, 7, 20],
-    't2': [2, 2, 5],
-    't3': [2, 8, 10],
+    't1': {'wcet': 3, 'period': 20, 'deadline': 7},
+    't2': {'wcet': 2, 'period': 5, 'deadline': 4},
+    't3': {'wcet': 2, 'period': 10, 'deadline': 8},
 }
 
 
@@ -23,21 +23,21 @@ def lcm(_list):
 
 
 def edf():
-    t_lcm = lcm([tasks[i][-1] for i in tasks])
+    t_lcm = lcm([tasks[i]['period'] for i in tasks])
 
-    t_dead = {i: tasks[i][1] for i in tasks}
+    t_dead = {i: tasks[i]['deadline'] for i in tasks}
 
     sorted_dead = sorted(t_dead.items(), key=lambda kv: (kv[1], kv[0]))
     # print(sorted_dead)
 
     ready_task = []
     for i in sorted_dead:
-        period = tasks[i[0]][-1]
+        period = tasks[i[0]]['period']
         # print('lcm: ', t_lcm, ' period: ', period)
         t_range = int(t_lcm/period)
         last_dead = 0
         for j in range(t_range):
-            ready_task.append((i[0], last_dead+tasks[i[0]][1]))
+            ready_task.append((i[0], last_dead+tasks[i[0]]['deadline']))
             last_dead += period
 
     ready_task = sorted(ready_task, key=lambda t: t[1])
@@ -48,14 +48,14 @@ def edf():
     missed = []
     register = {i: 0 for i in tasks.keys()}   # {ti : amount executed}
     for i in ready_task:
-        if (t_time//tasks[i[0]][-1])+1 <= register[i[0]]:
-            while (t_time//tasks[i[0]][-1])+1 <= register[i[0]]:
+        if (t_time//tasks[i[0]]['period'])+1 <= register[i[0]]:
+            while (t_time//tasks[i[0]]['period'])+1 <= register[i[0]]:
                 t_time += 1
                 schedule.append(('idle', t_time))
-        if (t_time//tasks[i[0]][-1])+1 > register[i[0]]:
-            if t_time + tasks[i[0]][0] <= i[1]:
+        if (t_time//tasks[i[0]]['period'])+1 > register[i[0]]:
+            if t_time + tasks[i[0]]['wcet'] <= i[1]:
                 register[i[0]] += 1
-                t_time += tasks[i[0]][0]
+                t_time += tasks[i[0]]['wcet']
                 schedule.append((i[0], t_time))
             else:
                 print('Deadline missed: ', i)
