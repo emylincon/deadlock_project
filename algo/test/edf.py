dc28042a26b904b285b4244b3833a35323b679ef
Emeka Ugwuanyi <emylincon@gmail.com>
2019-05-31 20:05:05 +0100

    finishing scheduler

--- algo/test/edf.py
+++ algo/test/edf.py
@@ -26,5 +26,38 @@ t_lcm = lcm([tasks[i][-1] for i in tasks])
 
 t_dead = {i: tasks[i][1] for i in tasks}
 
-print(sorted(t_dead.items(), key =
-             lambda kv:(kv[1], kv[0])))
+sorted_dead = sorted(t_dead.items(), key=lambda kv: (kv[1], kv[0]))
+# print(sorted_dead)
+
+ready_task = []
+for i in sorted_dead:
+    period = tasks[i[0]][-1]
+    # print('lcm: ', t_lcm, ' period: ', period)
+    t_range = int(t_lcm/period)
+    last_dead = 0
+    for j in range(t_range):
+        ready_task.append((i[0], last_dead+tasks[i[0]][1]))
+        last_dead += period
+
+
+ready_task = sorted(ready_task, key=lambda t: t[1])
+print(ready_task)
+
+t_time = 0
+schedule = []
+register = {i: 0 for i in tasks.keys()}   # {ti : amount executed}
+for i in ready_task:
+    if (t_time//tasks[i[0]][-1])+1 <= register[i[0]]:
+        while (t_time//tasks[i[0]][-1])+1 <= register[i[0]]:
+            t_time += 1
+            schedule.append(('idle', t_time))
+    if (t_time//tasks[i[0]][-1])+1 > register[i[0]]:
+        if t_time + tasks[i[0]][0] <= i[1]:
+            register[i[0]] += 1
+            t_time += tasks[i[0]][0]
+            schedule.append((i[0], t_time))
+        else:
+            print('Deadline missed: ', i)
+
+print('s: ', schedule)
+print('r: ', register)
