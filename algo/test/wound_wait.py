3c0c5f92446957281e15d5188cfb825d0f6f9b42
Emeka Ugwuanyi <emylincon@gmail.com>
2019-06-09 18:01:15 +0100

    added wound_wait algorithm

---
+++ algo/test/wound_wait.py
@@ -0,0 +1,132 @@
+# https://www.geeksforgeeks.org/program-bankers-algorithm-set-1-safety-algorithm/
+# Python3 program to illustrate
+# Banker's Algorithm
+import numpy as np
+# work = 3 3 2
+_need = {
+    'p0': [7, 4, 3],
+    'p1': [1, 2, 2],
+    'p2': [6, 0, 0],
+    'p3': [0, 1, 1],
+    'p4': [4, 3, 1]
+
+}
+
+allocation = {
+    'p0': [0, 1, 0],
+    'p1': [2, 0, 0],
+    'p2': [3, 0, 2],
+    'p3': [2, 1, 1],
+    'p4': [0, 0, 2]
+}
+
+# Number of processes
+P = len(allocation)
+
+# Number of resources
+R = 3
+
+
+# safe state or not
+def isSafe(processes, avail, maxm, allot):
+    need = [_need[i] for i in _need]
+    offload = []
+
+    # Mark all processes as infinish
+    finish = [0] * P
+
+    # To store safe sequence
+    safeSeq = [0] * P
+
+    # Make a copy of available resources
+    work = [0] * R
+    for i in range(R):
+        work[i] = avail[i]
+
+        # While all processes are not finished
+    # or system is not in safe state.
+    count = 0
+    while (count < P):
+
+        # Find a process which is not finish
+        # and whose needs can be satisfied
+        # with current work[] resources.
+        found = False
+        for p in range(P):
+
+            # First check if a process is finished,
+            # if no, go for next condition
+            if (finish[p] == 0):
+
+                # Check if for all resources
+                # of current P need is less
+                # than work
+                for j in range(R):
+                    if (need[p][j] > work[j]):
+                        break
+
+                # If all needs of p were satisfied.
+                if (j == R - 1):
+
+                    # Add the allocated resources of
+                    # current P to the available/work
+                    # resources i.e.free the resources
+                    for k in range(R):
+                        work[k] += allot[p][k]
+
+                        # Add this process to safe sequence.
+                    safeSeq[count] = processes[p]
+                    count += 1
+
+                    # Mark this p as finished
+                    finish[p] = 1
+
+                    found = True
+
+        # If we could not find a next process
+        # in safe sequence.
+        if (found == False):
+            print("System is not in safe state")
+
+            a = list(set(processes) - set(safeSeq) - set(offload))
+            _max = np.array([0, 0, 0])
+            n = {}
+            for i in a:
+                n[i] = sum(allocation[i])
+            _max = max(n, key=n.get)
+            print('work: ', work, 'need: ', _need[_max])
+            offload.append(_max)
+            work = np.array(work) + np.array(allocation[_max])
+            count += 1
+
+            # Mark this p as finished
+            finish[processes.index(_max)] = 1
+            found = True
+
+
+    # If system is in safe state then
+    # safe sequence will be as below
+    print("System is in safe state.",
+          "\nSafe sequence is: ", end=" ")
+    print(*safeSeq)
+    print(offload)
+
+    return True
+
+
+# Driver code
+if __name__ == "__main__":
+    processes = ['p0', 'p1', 'p2', 'p3', 'p4']
+
+    # Available instances of resources
+    avail = [3, 1, 2]
+
+    # Maximum R that can be allocated
+    # to processes
+    maxm = [np.array(allocation[i]) + np.array(_need[i]) for i in _need]
+
+    # Resources allocated to processes
+    allot = [allocation[i] for i in allocation]
+
+    # Check system is in safe state or not
+    isSafe(processes, avail, maxm, allot)
