2a27b5b06884ddefda0be10b46db73e0d5dc4424
Emeka Ugwuanyi <emylincon@gmail.com>
2019-07-18 21:32:45 +0100

    setup file updated

--- setup.sh
+++ setup.sh
@@ -4,34 +4,34 @@ clear
 echo '============ Preparing your MEC Platform =============='
 sleep 3
 echo -e "\nupdating repository...\n"
-apt update && apt upgrade -y >> /dev/null
+apt update && apt upgrade -y
 
 echo -e "installing python3...\n"
-apt install python3 -y >> /dev/null
-apt install python3-pip -y >> /dev/null
+apt install python3 -y
+apt install python3-pip -y
 
 echo "installing required python3 packages..."
 echo -e "(drawnow, psutil, matplotlib, paramiko, pyfiglet, numpy)\n"
-pip3 install drawnow >> /dev/null
-apt install python3-psutil -y >> /dev/null
-apt install python3-matplotlib -y >> /dev/null
-apt install python3-paramiko -y >> /dev/null
-apt install python3-pyfiglet -y >> /dev/null
-apt install python3-numpy -y >> /dev/null
+pip3 install drawnow
+apt install python3-psutil -y
+apt install python3-matplotlib -y
+apt install python3-paramiko -y
+apt install python3-pyfiglet -y
+apt install python3-numpy -y
 
 echo -e "installing openssh...\n"
-apt install openssh-client -y >> /dev/null
-apt install openssh-server -y >> /dev/null
+apt install openssh-client -y
+apt install openssh-server -y
 
 echo "installing network tools..."
 echo -e "(wget, apt-utils, iputils-ping, net-tools, nmap)\n"
-apt install wget -y >> /dev/null
-apt install apt-utils -y >> /dev/null
-apt install iputils-ping -y >> /dev/null
-apt install net-tools -y >> /dev/null
-apt install nano -y >> /dev/null
-apt install nmap -y >> /dev/null
-
+apt install wget -y
+apt install apt-utils -y
+apt install iputils-ping -y
+apt install net-tools -y
+apt install nano -y
+apt install nmap -y
+clear
 echo -e "starting ssh server..\n"
 sleep 1.5
 /etc/init.d/ssh start
