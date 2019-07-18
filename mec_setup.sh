231c6b0c32ae23a33d43b9d71c769bcd5d1da729
Emeka Ugwuanyi <emylincon@gmail.com>
2019-07-18 22:15:48 +0100

    creating a setup file for installation on alpine linux

--- mec_setup.sh
+++ mec_setup.sh
@@ -5,22 +5,21 @@ echo '============ Preparing your MEC Platform =============='
 sleep 3
 apk update && apk upgrade
 apk add python3
-apk add python3-pip -y
-pip3 install drawnow -y
-apt install python3-psutil -y
-apt install python3-matplotlib -y
-apt install python3-paramiko -y
-apt install python3-pyfiglet -y
-apt install openssh-client -y
-apt install openssh-server -y
-apt install wget -y
-apt install apt-utils -y
-apt install iputils-ping -y
-apt install net-tools -y
-apt install nano -y
-apt install python3-numpy -y
-apt install nmap -y
+
+pip3 install psutil
+pip3 install paramiko
+pip3 install numpy
+
+apk add openssh-client
+apk add openssh
+
+apk add wget
+apk add nano
+apk add net-tools
+apk add nmap
 
 /etc/init.d/ssh start
 clear
 echo '============= All done.. Ready to use! ==============='
+
+#pip3 install matplotlib
