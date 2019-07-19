9baf1bf435dc23901e9f5a5449c94daf1e2f6f9e
Emeka Ugwuanyi <emylincon@gmail.com>
2019-07-19 20:07:32 +0100

    creating a setup file for installation on alpine linux

--- mec_setup.sh
+++ mec_setup.sh
@@ -4,7 +4,34 @@ clear
 echo '============ Preparing your MEC Platform =============='
 sleep 3
 apk update && apk upgrade
+apk add busybox-static apk-tools-static
+apk add make
+cd
+apk add git
+clear
+
+
+echo "upgrading to alpine 3.10"
+sleep 2
+git clone https://github.com/alpinelinux/alpine-conf.git
+cd alpine-conf
+make
+mv libalpine.sh /lib/
+sh setup-apkrepos
+r
+apk update
+sed -i -e 's/v3\.3/v3.10/g' /etc/apk/repositories
+apk.static update
+apk.static upgrade --no-self-upgrade --available --simulate
+clear
+
+echo "installing python3"
+sleep 2
 apk add python3
+apk add pkgconf build-base python3-dev
+apk add --no-cache --virtual .build-deps g++ python3-dev libffi-dev openssl-dev
+python3.7 -m ensurepip
+python3.7 -m pip install --upgrade pip
 
 pip3 install psutil
 pip3 install paramiko
@@ -12,6 +39,8 @@ pip3 install numpy
 
 apk add openssh-client
 apk add openssh
+apk add acf-openssh
+
 
 apk add wget
 apk add nano
