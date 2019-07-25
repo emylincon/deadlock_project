14482edaed1f96d2bbd15a36f26c84f4d899c5af
Emeka Ugwuanyi <emylincon@gmail.com>
2019-07-25 16:00:50 +0100

    creating functions

---
+++ algo/Experiment_data/rtt_compare.py
@@ -0,0 +1,23 @@
+from drawnow import *
+from matplotlib import pyplot as plt
+import data
+
+fig = plt.figure()
+ax1 = fig.add_subplot(361)
+ax2 = fig.add_subplot(362)
+ax3 = fig.add_subplot(363)
+ax4 = fig.add_subplot(364)
+ax5 = fig.add_subplot(365)
+ax6 = fig.add_subplot(366)
+ax7 = fig.add_subplot(367)
+ax8 = fig.add_subplot(368)
+ax9 = fig.add_subplot(3,6,9)
+ax10 = fig.add_subplot(3,6,10)
+ax11 = fig.add_subplot(3,6,11)
+ax12 = fig.add_subplot(3,6,12)
+ax13 = fig.add_subplot(3,6,13)
+ax14 = fig.add_subplot(3,6,14)
+ax15 = fig.add_subplot(3,6,15)
+ax16 = fig.add_subplot(3,6,16)
+ax17 = fig.add_subplot(3,6,17)
+ax18 = fig.add_subplot(3,6,18)
