9bf4366a14f04d366f0dcea3ca5dd7f6c65e31fe
Emeka Ugwuanyi <emylincon@gmail.com>
2019-08-15 21:46:23 +0100

    troubleshooting

--- 2algo/0_cloud.py
+++ 2algo/0_cloud.py
@@ -44,12 +44,17 @@ def receive_tasks_client(_con, _addr):   # run as thread
     with _con:
         print('Connected: ', _addr)
         while True:
-            data = _con.recv(1024)
-            # print(_addr[0], ': ', data.decode())
-            received_task = ast.literal_eval(data.decode())
-            received_task_queue[0].append(received_task[0])
-            received_task_queue[1][received_task[0]] = received_task[1]
-            cloud_register[received_task[0].split('.')[2]] = _addr[0]
+            try:
+                data = _con.recv(1024)
+                # print(_addr[0], ': ', data.decode())
+                d = str(data.decode())
+                print('d: ', d)
+                received_task = ast.literal_eval(d)
+                received_task_queue[0].append(received_task[0])
+                received_task_queue[1][received_task[0]] = received_task[1]
+                cloud_register[received_task[0].split('.')[2]] = _addr[0]
+            except Exception as e:
+                print('Error Encountered')
 
 
 def receive_connection():
