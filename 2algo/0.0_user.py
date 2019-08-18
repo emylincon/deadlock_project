5d4a36430125a1f432266c27c1fed396810ea539
Emeka Ugwuanyi <emylincon@gmail.com>
2019-08-18 12:47:17 +0100

    troubleshooting

--- 2algo/0.0_user.py
+++ 2algo/0.0_user.py
@@ -159,6 +159,7 @@ def receive_data(_con, _addr):
                                                  int(a[2]), int(a[3]),
                                                  int(a[4]), int(a[5]),
                                                  int(a[6])))
+                            print('value: {}'.format(k[2] - k[1]))
                             if k[2] - k[1] < k[0]:
                                 tasks_executed_on_time += 1
                             else:
@@ -169,6 +170,7 @@ def receive_data(_con, _addr):
                                             int(a[2]), int(a[3]),
                                             int(a[4]), int(a[5]),
                                             int(a[6]))
+                            print('value: {}'.format(t - k[1]))
                             if t - k[1] < k[0]:
                                 tasks_executed_on_time += 1
                             else:
