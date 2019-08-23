ad623dad3a2b3f301881e857a1519f0074f9ae78
Emeka Ugwuanyi <emylincon@gmail.com>
2019-08-23 11:04:31 +0100

    added a gui user file

--- 2algo/gui_user.py
+++ 2algo/gui_user.py
@@ -351,7 +351,7 @@ def main():
         try:
             x = input('Enter "y" to start and "stop" to exit: ').strip().lower()
             if x == 'y':
-                for i in range(500):
+                for i in range(100):
                     i = seq
                     rand_host = hosts[gosh_dist(len(hosts))]      # randomly selecting a host to send task to
                     _task_ = get_tasks()                 # tasks, waiting time
