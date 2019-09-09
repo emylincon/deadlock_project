f0975d3afec9a477f5c226667143cdde44136d5d
Emeka Ugwuanyi <emylincon@gmail.com>
2019-09-09 16:28:50 +0100

    adding data to file

---
+++ 2algo/results and data complilation/results_data.py
@@ -0,0 +1,20 @@
+import datetime
+
+# edf+wait_die
+timely5_16_4 = 2650
+untimely5_16_4 = 926
+
+timely5_16_5 = 2937
+untimely5_16_5 = 651
+
+timely5_16_6 = 2953
+untimely5_16_6 = 734
+
+timely4_16_4 = 2789
+untimely4_16_4 = 841
+
+timely4_16_5 = 3179
+untimely4_16_5 = 586
+
+timely4_16_6 = 2721
+untimely4_16_6 = 808
