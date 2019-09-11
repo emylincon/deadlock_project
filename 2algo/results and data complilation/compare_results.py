99970041da20b8c865668a6ced5296b57d291c52
Emeka Ugwuanyi <emylincon@gmail.com>
2019-09-11 17:12:51 +0100

    making data structure

---
+++ 2algo/results and data complilation/compare_results.py
@@ -0,0 +1,93 @@
+import results_data as rd
+
+
+data = {2: {'timely_2': [rd.timely3_2_4, rd.timely4_2_4, rd.timely5_2_4, rd.timely3_2_5, rd.timely4_2_5,
+                     rd.timely5_2_5,
+                     rd.timely3_2_6,
+                     rd.timely4_2_6,
+                     rd.timely5_2_6,
+                     ],
+        'untimely_2': [rd.untimely3_2_4, rd.untimely4_2_4, rd.untimely5_2_4, rd.untimely3_2_5, rd.untimely4_2_5,
+                       rd.untimely5_2_5,
+                       rd.untimely3_2_6,
+                       rd.untimely4_2_6,
+                       rd.untimely5_2_6,
+                       ]
+            },
+        3: {
+        'timely_3': [rd.timely3_3_4, rd.timely4_3_4, rd.timely5_3_4, rd.timely3_3_5, rd.timely4_3_5,
+                     rd.timely5_3_5,
+                     rd.timely3_3_6,
+                     rd.timely4_3_6,
+                     rd.timely5_3_6,
+                     ],
+        'untimely_3': [rd.untimely3_3_4, rd.untimely4_3_4, rd.untimely5_3_4, rd.untimely3_3_5, rd.untimely4_3_5,
+                       rd.untimely5_3_5,
+                       rd.untimely3_3_6,
+                       rd.untimely4_3_6,
+                       rd.untimely5_3_6,
+                       ]
+        },
+        7: {
+        'timely_7': [rd.timely3_7_4, rd.timely4_7_4, rd.timely5_7_4, rd.timely3_7_5, rd.timely4_7_5,
+                     rd.timely5_7_5,
+                     rd.timely3_7_6,
+                     rd.timely4_7_6,
+                     rd.timely5_7_6,
+                     ],
+        'untimely_7': [rd.untimely3_7_4, rd.untimely4_7_4, rd.untimely5_7_4, rd.untimely3_7_5, rd.untimely4_7_5,
+                       rd.untimely5_7_5,
+                       rd.untimely3_7_6,
+                       rd.untimely4_7_6,
+                       rd.untimely5_7_6,
+                       ],
+        },
+        10: {
+        'timely_10': [rd.timely3_10_4, rd.timely4_10_4, rd.timely5_10_4, rd.timely3_10_5, rd.timely4_10_5,
+                      rd.timely5_10_5,
+                      rd.timely3_10_6,
+                      rd.timely4_10_6,
+                      rd.timely5_10_6,
+                      ],
+        'untimely_10': [rd.untimely3_10_4, rd.untimely4_10_4, rd.untimely5_10_4, rd.untimely3_10_5, rd.untimely4_10_5,
+                        rd.untimely5_10_5,
+                        rd.untimely3_10_6,
+                        rd.untimely4_10_6,
+                        rd.untimely5_10_6,
+                        ],
+        },
+        12: {
+        'timely_12': [rd.timely3_12_4, rd.timely4_12_4, rd.timely5_12_4, rd.timely3_12_5, rd.timely4_12_5,
+                      rd.timely5_12_5,
+                      rd.timely3_12_6,
+                      rd.timely4_12_6,
+                      rd.timely5_12_6,
+                      ],
+        'untimely_12': [rd.untimely3_12_4, rd.untimely4_12_4, rd.untimely5_12_4, rd.untimely3_12_5, rd.untimely4_12_5,
+                        rd.untimely5_12_5,
+                        rd.untimely3_12_6,
+                        rd.untimely4_12_6,
+                        rd.untimely5_12_6,
+                        ],
+        },
+        16: {
+        'timely_16': [rd.timely3_16_4, rd.timely4_16_4, rd.timely5_16_4, rd.timely3_16_5, rd.timely4_16_5,
+                      rd.timely5_16_5,
+                      rd.timely3_16_6,
+                      rd.timely4_16_6,
+                      rd.timely5_16_6,
+                      ],
+        'untimely_16': [rd.untimely3_16_4, rd.untimely4_16_4, rd.untimely5_16_4, rd.untimely3_16_5, rd.untimely4_16_5,
+                        rd.untimely5_16_5,
+                        rd.untimely3_16_6,
+                        rd.untimely4_16_6,
+                        rd.untimely5_16_6,
+                        ],
+        }
+        }
+
+
+
+def average_timely():
+    for
+
