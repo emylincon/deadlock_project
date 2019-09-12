2a26f014ea59070805dc3099973f7a225f4662e6
Emeka Ugwuanyi <emylincon@gmail.com>
2019-09-12 16:45:12 +0100

    adding data structure

--- 2algo/results and data complilation/compare_results.py
+++ 2algo/results and data complilation/compare_results.py
@@ -1,96 +1,94 @@
 import results_data as rd
 
-
 _time = {2: {
-        'timely': [rd.timely3_2_4, rd.timely4_2_4, rd.timely5_2_4, rd.timely3_2_5, rd.timely4_2_5,
-                        rd.timely5_2_5,
-                        rd.timely3_2_6,
-                        rd.timely4_2_6,
-                        rd.timely5_2_6,
-                        ],
-        'untimely': [rd.untimely3_2_4, rd.untimely4_2_4, rd.untimely5_2_4, rd.untimely3_2_5, rd.untimely4_2_5,
-                       rd.untimely5_2_5,
-                       rd.untimely3_2_6,
-                       rd.untimely4_2_6,
-                       rd.untimely5_2_6,
-                       ]
-             },
-         3: {
+    'timely': [rd.timely3_2_4, rd.timely4_2_4, rd.timely5_2_4, rd.timely3_2_5, rd.timely4_2_5,
+               rd.timely5_2_5,
+               rd.timely3_2_6,
+               rd.timely4_2_6,
+               rd.timely5_2_6,
+               ],
+    'untimely': [rd.untimely3_2_4, rd.untimely4_2_4, rd.untimely5_2_4, rd.untimely3_2_5, rd.untimely4_2_5,
+                 rd.untimely5_2_5,
+                 rd.untimely3_2_6,
+                 rd.untimely4_2_6,
+                 rd.untimely5_2_6,
+                 ]
+},
+    3: {
         'timely': [rd.timely3_3_4, rd.timely4_3_4, rd.timely5_3_4, rd.timely3_3_5, rd.timely4_3_5,
-                     rd.timely5_3_5,
-                     rd.timely3_3_6,
-                     rd.timely4_3_6,
-                     rd.timely5_3_6,
-                     ],
+                   rd.timely5_3_5,
+                   rd.timely3_3_6,
+                   rd.timely4_3_6,
+                   rd.timely5_3_6,
+                   ],
         'untimely': [rd.untimely3_3_4, rd.untimely4_3_4, rd.untimely5_3_4, rd.untimely3_3_5, rd.untimely4_3_5,
-                       rd.untimely5_3_5,
-                       rd.untimely3_3_6,
-                       rd.untimely4_3_6,
-                       rd.untimely5_3_6,
-                       ]
-        },
-         7: {
+                     rd.untimely5_3_5,
+                     rd.untimely3_3_6,
+                     rd.untimely4_3_6,
+                     rd.untimely5_3_6,
+                     ]
+    },
+    7: {
         'timely': [rd.timely3_7_4, rd.timely4_7_4, rd.timely5_7_4, rd.timely3_7_5, rd.timely4_7_5,
-                     rd.timely5_7_5,
-                     rd.timely3_7_6,
-                     rd.timely4_7_6,
-                     rd.timely5_7_6,
-                     ],
+                   rd.timely5_7_5,
+                   rd.timely3_7_6,
+                   rd.timely4_7_6,
+                   rd.timely5_7_6,
+                   ],
         'untimely': [rd.untimely3_7_4, rd.untimely4_7_4, rd.untimely5_7_4, rd.untimely3_7_5, rd.untimely4_7_5,
-                       rd.untimely5_7_5,
-                       rd.untimely3_7_6,
-                       rd.untimely4_7_6,
-                       rd.untimely5_7_6,
-                       ],
-        },
-         10: {
+                     rd.untimely5_7_5,
+                     rd.untimely3_7_6,
+                     rd.untimely4_7_6,
+                     rd.untimely5_7_6,
+                     ],
+    },
+    10: {
         'timely': [rd.timely3_10_4, rd.timely4_10_4, rd.timely5_10_4, rd.timely3_10_5, rd.timely4_10_5,
-                      rd.timely5_10_5,
-                      rd.timely3_10_6,
-                      rd.timely4_10_6,
-                      rd.timely5_10_6,
-                      ],
+                   rd.timely5_10_5,
+                   rd.timely3_10_6,
+                   rd.timely4_10_6,
+                   rd.timely5_10_6,
+                   ],
         'untimely': [rd.untimely3_10_4, rd.untimely4_10_4, rd.untimely5_10_4, rd.untimely3_10_5, rd.untimely4_10_5,
-                        rd.untimely5_10_5,
-                        rd.untimely3_10_6,
-                        rd.untimely4_10_6,
-                        rd.untimely5_10_6,
-                        ],
-        },
-         12: {
+                     rd.untimely5_10_5,
+                     rd.untimely3_10_6,
+                     rd.untimely4_10_6,
+                     rd.untimely5_10_6,
+                     ],
+    },
+    12: {
         'timely': [rd.timely3_12_4, rd.timely4_12_4, rd.timely5_12_4, rd.timely3_12_5, rd.timely4_12_5,
-                      rd.timely5_12_5,
-                      rd.timely3_12_6,
-                      rd.timely4_12_6,
-                      rd.timely5_12_6,
-                      ],
+                   rd.timely5_12_5,
+                   rd.timely3_12_6,
+                   rd.timely4_12_6,
+                   rd.timely5_12_6,
+                   ],
         'untimely': [rd.untimely3_12_4, rd.untimely4_12_4, rd.untimely5_12_4, rd.untimely3_12_5, rd.untimely4_12_5,
-                        rd.untimely5_12_5,
-                        rd.untimely3_12_6,
-                        rd.untimely4_12_6,
-                        rd.untimely5_12_6,
-                        ],
-        },
-         16: {
+                     rd.untimely5_12_5,
+                     rd.untimely3_12_6,
+                     rd.untimely4_12_6,
+                     rd.untimely5_12_6,
+                     ],
+    },
+    16: {
         'timely': [rd.timely3_16_4, rd.timely4_16_4, rd.timely5_16_4, rd.timely3_16_5, rd.timely4_16_5,
-                      rd.timely5_16_5,
-                      rd.timely3_16_6,
-                      rd.timely4_16_6,
-                      rd.timely5_16_6,
-                      ],
+                   rd.timely5_16_5,
+                   rd.timely3_16_6,
+                   rd.timely4_16_6,
+                   rd.timely5_16_6,
+                   ],
         'untimely': [rd.untimely3_16_4, rd.untimely4_16_4, rd.untimely5_16_4, rd.untimely3_16_5, rd.untimely4_16_5,
-                        rd.untimely5_16_5,
-                        rd.untimely3_16_6,
-                        rd.untimely4_16_6,
-                        rd.untimely5_16_6,
-                        ],
-        }
-         }
-
+                     rd.untimely5_16_5,
+                     rd.untimely3_16_6,
+                     rd.untimely4_16_6,
+                     rd.untimely5_16_6,
+                     ],
+    }
+}
 
 wait_time = {
     24: [rd.wt0_2_4, rd.wt1_2_4, rd.wt2_2_4, rd.wt3_2_4],
-    25: [rd.wt0_2_5, rd.wt1_2_5, rd.wt2_2_5, rd.wt3_2_5, rd.wt4_2_5,],
+    25: [rd.wt0_2_5, rd.wt1_2_5, rd.wt2_2_5, rd.wt3_2_5, rd.wt4_2_5, ],
     26: [rd.wt0_2_6, rd.wt1_2_6, rd.wt2_2_6, rd.wt3_2_6, rd.wt4_2_6, rd.wt5_2_6],
 
     34: [rd.wt0_3_4, rd.wt1_3_4, rd.wt2_3_4, rd.wt3_3_4],
@@ -116,7 +114,7 @@ wait_time = {
 
 _rtt_ = {
     24: [rd.rtt0_2_4, rd.rtt1_2_4, rd.rtt2_2_4, rd.rtt3_2_4],
-    25: [rd.rtt0_2_5, rd.rtt1_2_5, rd.rtt2_2_5, rd.rtt3_2_5, rd.rtt4_2_5,],
+    25: [rd.rtt0_2_5, rd.rtt1_2_5, rd.rtt2_2_5, rd.rtt3_2_5, rd.rtt4_2_5, ],
     26: [rd.rtt0_2_6, rd.rtt1_2_6, rd.rtt2_2_6, rd.rtt3_2_6, rd.rtt4_2_6, rd.rtt5_2_6],
 
     34: [rd.rtt0_3_4, rd.rtt1_3_4, rd.rtt2_3_4, rd.rtt3_3_4],
@@ -140,10 +138,9 @@ _rtt_ = {
     166: [rd.rtt0_16_6, rd.rtt1_16_6, rd.rtt2_16_6, rd.rtt3_16_6, rd.rtt4_16_6, rd.rtt5_16_6],
 }
 
-
 _cpu_ = {
     24: [rd.cpu0_2_4, rd.cpu1_2_4, rd.cpu2_2_4, rd.cpu3_2_4],
-    25: [rd.cpu0_2_5, rd.cpu1_2_5, rd.cpu2_2_5, rd.cpu3_2_5, rd.cpu4_2_5,],
+    25: [rd.cpu0_2_5, rd.cpu1_2_5, rd.cpu2_2_5, rd.cpu3_2_5, rd.cpu4_2_5, ],
     26: [rd.cpu0_2_6, rd.cpu1_2_6, rd.cpu2_2_6, rd.cpu3_2_6, rd.cpu4_2_6, rd.cpu5_2_6],
 
     34: [rd.cpu0_3_4, rd.cpu1_3_4, rd.cpu2_3_4, rd.cpu3_3_4],
@@ -169,7 +166,7 @@ _cpu_ = {
 
 _loc_ = {
     24: [rd.loc0_2_4, rd.loc1_2_4, rd.loc2_2_4, rd.loc3_2_4],
-    25: [rd.loc0_2_5, rd.loc1_2_5, rd.loc2_2_5, rd.loc3_2_5, rd.loc4_2_5,],
+    25: [rd.loc0_2_5, rd.loc1_2_5, rd.loc2_2_5, rd.loc3_2_5, rd.loc4_2_5, ],
     26: [rd.loc0_2_6, rd.loc1_2_6, rd.loc2_2_6, rd.loc3_2_6, rd.loc4_2_6, rd.loc5_2_6],
 
     34: [rd.loc0_3_4, rd.loc1_3_4, rd.loc2_3_4, rd.loc3_3_4],
@@ -193,37 +190,41 @@ _loc_ = {
     166: [rd.loc0_16_6, rd.loc1_16_6, rd.loc2_16_6, rd.loc3_16_6, rd.loc4_16_6, rd.loc5_16_6],
 }
 
-
 _off_cloud_ = {
     24: [rd.off_cloud0_2_4, rd.off_cloud1_2_4, rd.off_cloud2_2_4, rd.off_cloud3_2_4],
-    25: [rd.off_cloud0_2_5, rd.off_cloud1_2_5, rd.off_cloud2_2_5, rd.off_cloud3_2_5, rd.off_cloud4_2_5,],
-    26: [rd.off_cloud0_2_6, rd.off_cloud1_2_6, rd.off_cloud2_2_6, rd.off_cloud3_2_6, rd.off_cloud4_2_6, rd.off_cloud5_2_6],
+    25: [rd.off_cloud0_2_5, rd.off_cloud1_2_5, rd.off_cloud2_2_5, rd.off_cloud3_2_5, rd.off_cloud4_2_5, ],
+    26: [rd.off_cloud0_2_6, rd.off_cloud1_2_6, rd.off_cloud2_2_6, rd.off_cloud3_2_6, rd.off_cloud4_2_6,
+         rd.off_cloud5_2_6],
 
     34: [rd.off_cloud0_3_4, rd.off_cloud1_3_4, rd.off_cloud2_3_4, rd.off_cloud3_3_4],
     35: [rd.off_cloud0_3_5, rd.off_cloud1_3_5, rd.off_cloud2_3_5, rd.off_cloud3_3_5, rd.off_cloud4_3_5],
-    36: [rd.off_cloud0_3_6, rd.off_cloud1_3_6, rd.off_cloud2_3_6, rd.off_cloud3_3_6, rd.off_cloud4_3_6, rd.off_cloud5_3_6],
+    36: [rd.off_cloud0_3_6, rd.off_cloud1_3_6, rd.off_cloud2_3_6, rd.off_cloud3_3_6, rd.off_cloud4_3_6,
+         rd.off_cloud5_3_6],
 
     74: [rd.off_cloud0_7_4, rd.off_cloud1_7_4, rd.off_cloud2_7_4, rd.off_cloud3_7_4],
     75: [rd.off_cloud0_7_5, rd.off_cloud1_7_5, rd.off_cloud2_7_5, rd.off_cloud3_7_5, rd.off_cloud4_7_5],
-    76: [rd.off_cloud0_7_6, rd.off_cloud1_7_6, rd.off_cloud2_7_6, rd.off_cloud3_7_6, rd.off_cloud4_7_6, rd.off_cloud5_7_6],
+    76: [rd.off_cloud0_7_6, rd.off_cloud1_7_6, rd.off_cloud2_7_6, rd.off_cloud3_7_6, rd.off_cloud4_7_6,
+         rd.off_cloud5_7_6],
 
     104: [rd.off_cloud0_10_4, rd.off_cloud1_10_4, rd.off_cloud2_10_4, rd.off_cloud3_10_4],
     105: [rd.off_cloud0_10_5, rd.off_cloud1_10_5, rd.off_cloud2_10_5, rd.off_cloud3_10_5, rd.off_cloud4_10_5],
-    106: [rd.off_cloud0_10_6, rd.off_cloud1_10_6, rd.off_cloud2_10_6, rd.off_cloud3_10_6, rd.off_cloud4_10_6, rd.off_cloud5_10_6],
+    106: [rd.off_cloud0_10_6, rd.off_cloud1_10_6, rd.off_cloud2_10_6, rd.off_cloud3_10_6, rd.off_cloud4_10_6,
+          rd.off_cloud5_10_6],
 
     124: [rd.off_cloud0_12_4, rd.off_cloud1_12_4, rd.off_cloud2_12_4, rd.off_cloud3_12_4],
     125: [rd.off_cloud0_12_5, rd.off_cloud1_12_5, rd.off_cloud2_12_5, rd.off_cloud3_12_5, rd.off_cloud4_12_5],
-    126: [rd.off_cloud0_12_6, rd.off_cloud1_12_6, rd.off_cloud2_12_6, rd.off_cloud3_12_6, rd.off_cloud4_12_6, rd.off_cloud5_12_6],
+    126: [rd.off_cloud0_12_6, rd.off_cloud1_12_6, rd.off_cloud2_12_6, rd.off_cloud3_12_6, rd.off_cloud4_12_6,
+          rd.off_cloud5_12_6],
 
     164: [rd.off_cloud0_16_4, rd.off_cloud1_16_4, rd.off_cloud2_16_4, rd.off_cloud3_16_4],
     165: [rd.off_cloud0_16_5, rd.off_cloud1_16_5, rd.off_cloud2_16_5, rd.off_cloud3_16_5, rd.off_cloud4_16_5],
-    166: [rd.off_cloud0_16_6, rd.off_cloud1_16_6, rd.off_cloud2_16_6, rd.off_cloud3_16_6, rd.off_cloud4_16_6, rd.off_cloud5_16_6],
+    166: [rd.off_cloud0_16_6, rd.off_cloud1_16_6, rd.off_cloud2_16_6, rd.off_cloud3_16_6, rd.off_cloud4_16_6,
+          rd.off_cloud5_16_6],
 }
-
 
 _off_mec_ = {
     24: [rd.off_mec0_2_4, rd.off_mec1_2_4, rd.off_mec2_2_4, rd.off_mec3_2_4],
-    25: [rd.off_mec0_2_5, rd.off_mec1_2_5, rd.off_mec2_2_5, rd.off_mec3_2_5, rd.off_mec4_2_5,],
+    25: [rd.off_mec0_2_5, rd.off_mec1_2_5, rd.off_mec2_2_5, rd.off_mec3_2_5, rd.off_mec4_2_5, ],
     26: [rd.off_mec0_2_6, rd.off_mec1_2_6, rd.off_mec2_2_6, rd.off_mec3_2_6, rd.off_mec4_2_6, rd.off_mec5_2_6],
 
     34: [rd.off_mec0_3_4, rd.off_mec1_3_4, rd.off_mec2_3_4, rd.off_mec3_3_4],
@@ -248,7 +249,6 @@ _off_mec_ = {
 }
 
 
-
 def average_timely():
     av = {}
 
@@ -264,40 +264,5 @@ def average_timely():
 
     return av
 
+
 print(average_timely())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
