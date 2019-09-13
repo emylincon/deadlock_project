09e55fe25b7a18939d5ed97a41ba85a39c8ce3bc
Emeka Ugwuanyi <emylincon@gmail.com>
2019-09-13 19:44:19 +0100

    ploiting data

--- 2algo/results and data complilation/av_dataset.py
+++ 2algo/results and data complilation/av_dataset.py
@@ -1,50 +1,126 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-time_av = {'timely_4_2': 10374,
-           'timely_5_2': 11052,
-           'timely_6_2': 10861,
+fig = plt.figure()
+ax1 = fig.add_subplot(311)
+ax2 = fig.add_subplot(312)
+ax3 = fig.add_subplot(313)
+width = 0.35
+title = {2}
+time_av_t = {'_4_2': 10374,
+             '_5_2': 11052,
+             '_6_2': 10861,
 
-           'untimely_4_2': 544,
-           'untimely_5_2': 111,
-           'untimely_6_2': 211,
+             '_4_3': 6465,
+             '_5_3': 8200,
+             '_6_3': 10791,
 
-           'timely_4_3': 6465,
-           'timely_5_3': 8200,
-           'timely_6_3': 10791,
+             '_4_7': 10329,
+             '_5_7': 10951,
+             '_6_7': 10136,
 
-           'untimely_4_3': 4479,
-           'untimely_5_3': 2898,
-           'untimely_6_3': 165,
+             '_4_10': 6721,
+             '_5_10': 7418,
+             '_6_10': 10913,
 
-           'timely_4_7': 10329,
-           'timely_5_7': 10951,
-           'timely_6_7': 10136,
+             '_4_12': 9172,
+             '_5_12': 10441,
+             '_6_12': 9618,
 
-           'untimely_4_7': 580,
-           'untimely_5_7': 218,
-           'untimely_6_7': 919,
+             '_4_16': 8364,
+             '_5_16': 9225,
+             '_6_16': 8536,
 
-           'timely_4_10': 6721,
-           'timely_5_10': 7418,
-           'timely_6_10': 10913,
+             }
+time_av_u = {
+    '_4_2': 544,
+    '_5_2': 111,
+    '_6_2': 211,
 
-           'untimely_4_10': 4193,
-           'untimely_5_10': 3743,
-           'untimely_6_10': 170,
+    '_4_3': 4479,
+    '_5_3': 2898,
+    '_6_3': 165,
 
-           'timely_4_12': 9172,
-           'timely_5_12': 10441,
-           'timely_6_12': 9618,
+    '_4_7': 580,
+    '_5_7': 218,
+    '_6_7': 919,
 
-           'untimely_4_12': 1717,
-           'untimely_5_12': 651,
-           'untimely_6_12': 1337,
+    '_4_10': 4193,
+    '_5_10': 3743,
+    '_6_10': 170,
 
-           'timely_4_16': 8364,
-           'timely_5_16': 9225,
-           'timely_6_16': 8536,
+    '_4_12': 1717,
+    '_5_12': 651,
+    '_6_12': 1337,
 
-           'untimely_4_16': 2527,
-           'untimely_5_16': 1855,
-           'untimely_6_16': 2396
-           }
+    '_4_16': 2527,
+    '_5_16': 1855,
+    '_6_16': 2396
+
+}
+
+
+def histogram(timely, untimely, ax):
+    ind = np.arange(len(timely))
+    p1 = ax.bar(ind, untimely , width)
+    p2 = ax.bar(ind, timely , width, bottom=untimely)
+    ax.set_xticks(ind)
+    ax.set_xticklabels(('RMS + Bankers',
+                        'EDF + Bankers',
+                        'RMS + wound wait',
+                        'RMS + wait die',
+                        'EDF + wound wait',
+                        'EDF + wait die'))
+    ax.legend((p1[0], p2[0]), ('Untimely', 'Timely'))
+
+
+def format_data():
+    t_data = {}
+    u_data = {}
+    _keys = list(time_av_t.keys())
+    s4 = 0
+    s5 = 1
+    s6 = 2
+    for i in range(len(_keys)):
+        j = _keys[i]
+        if i == s4:
+            if 4 in t_data:
+                t_data[4].append(time_av_t[j])
+                u_data[4].append(time_av_u[j])
+                s4 += 3
+            else:
+                t_data[4] = [time_av_t[j]]
+                u_data[4] = [time_av_u[j]]
+                s4 += 3
+        elif i == s5:
+            if 5 in t_data:
+                t_data[5].append(time_av_t[j])
+                u_data[5].append(time_av_u[j])
+                s5 += 3
+            else:
+                t_data[5] = [time_av_t[j]]
+                u_data[5] = [time_av_u[j]]
+                s5 += 3
+        elif i == s6:
+            if 6 in t_data:
+                t_data[6].append(time_av_t[j])
+                u_data[6].append(time_av_u[j])
+                s6 += 3
+            else:
+                t_data[6] = [time_av_t[j]]
+                u_data[6] = [time_av_u[j]]
+                s6 += 3
+
+    return t_data, u_data
+
+            
+def plot_av_times():
+    axes = {ax1: 4, ax2: 5, ax3: 6}
+    _data = format_data()
+    for i in axes:
+        histogram(_data[0][axes[i]], _data[1][axes[i]], i)
+    fig.suptitle('MEC CPU Utilization During Deadlock Experiment')
+    plt.show()
+
+
+plot_av_times()
