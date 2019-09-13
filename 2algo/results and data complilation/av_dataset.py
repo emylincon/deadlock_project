af3db7dc8c4db626193917657d0286e418feabe5
Emeka Ugwuanyi <emylincon@gmail.com>
2019-09-13 14:26:59 +0100

    ploiting data

--- 2algo/results and data complilation/av_dataset.py
+++ 2algo/results and data complilation/av_dataset.py
@@ -1,49 +1,50 @@
+import matplotlib.pyplot as plt
 
 time_av = {'timely_4_2': 10374,
-      'timely_5_2': 11052,
-      'timely_6_2': 10861,
+           'timely_5_2': 11052,
+           'timely_6_2': 10861,
 
-      'untimely_4_2': 544,
-      'untimely_5_2': 111,
-      'untimely_6_2': 211,
+           'untimely_4_2': 544,
+           'untimely_5_2': 111,
+           'untimely_6_2': 211,
 
-      'timely_4_3': 6465,
-      'timely_5_3': 8200,
-      'timely_6_3': 10791,
+           'timely_4_3': 6465,
+           'timely_5_3': 8200,
+           'timely_6_3': 10791,
 
-      'untimely_4_3': 4479,
-      'untimely_5_3': 2898,
-      'untimely_6_3': 165,
+           'untimely_4_3': 4479,
+           'untimely_5_3': 2898,
+           'untimely_6_3': 165,
 
-      'timely_4_7': 10329,
-      'timely_5_7': 10951,
-      'timely_6_7': 10136,
+           'timely_4_7': 10329,
+           'timely_5_7': 10951,
+           'timely_6_7': 10136,
 
-      'untimely_4_7': 580,
-      'untimely_5_7': 218,
-      'untimely_6_7': 919,
+           'untimely_4_7': 580,
+           'untimely_5_7': 218,
+           'untimely_6_7': 919,
 
-      'timely_4_10': 6721,
-      'timely_5_10': 7418,
-      'timely_6_10': 10913,
+           'timely_4_10': 6721,
+           'timely_5_10': 7418,
+           'timely_6_10': 10913,
 
-      'untimely_4_10': 4193,
-      'untimely_5_10': 3743,
-      'untimely_6_10': 170,
+           'untimely_4_10': 4193,
+           'untimely_5_10': 3743,
+           'untimely_6_10': 170,
 
-      'timely_4_12': 9172,
-      'timely_5_12': 10441,
-      'timely_6_12': 9618,
+           'timely_4_12': 9172,
+           'timely_5_12': 10441,
+           'timely_6_12': 9618,
 
-      'untimely_4_12': 1717,
-      'untimely_5_12': 651,
-      'untimely_6_12': 1337,
+           'untimely_4_12': 1717,
+           'untimely_5_12': 651,
+           'untimely_6_12': 1337,
 
-      'timely_4_16': 8364,
-      'timely_5_16': 9225,
-      'timely_6_16': 8536,
+           'timely_4_16': 8364,
+           'timely_5_16': 9225,
+           'timely_6_16': 8536,
 
-      'untimely_4_16': 2527,
-      'untimely_5_16': 1855,
-      'untimely_6_16': 2396
-      }
+           'untimely_4_16': 2527,
+           'untimely_5_16': 1855,
+           'untimely_6_16': 2396
+           }
