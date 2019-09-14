d7423285d486b574b38046ccc5e0edaada21fe3f
Emeka Ugwuanyi <emylincon@gmail.com>
2019-09-14 17:04:29 +0100

    fixing error

--- 2algo/4_edf_bankers_gui.py
+++ 2algo/4_edf_bankers_gui.py
@@ -880,6 +880,7 @@ def start_loop():
                 print('\nProgramme Terminated')
                 result = f"wt_3_{mec_no} = {mec_waiting_time} \nrtt_3_{mec_no} = {mec_rtt} \ncpu_3_{mec_no} = {_cpu} " \
                          f"\noff_mec3_{mec_no} = {_off_mec} \noff_cloud3_{mec_no} = {_off_cloud} " \
+                         f"\ninward_mec = {_inward_mec}" \
                          f"\nloc3_{mec_no} = {_loc} \ndeadlock3_{mec_no} = {deadlock} \nmemory3_{mec_no} = {memory}"
                 cmd = 'echo "{}" >> data.py'.format(result)
                 os.system(cmd)
