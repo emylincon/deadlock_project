71a1e0ece7a193e75a23682de63f10eb4426141c
Emeka Ugwuanyi <emylincon@gmail.com>
2019-09-17 18:24:33 +0100

    rms wait die done

--- 2algo/8_rms_wait_die_Speak.py
+++ 2algo/8_rms_wait_die_Speak.py
@@ -689,10 +689,10 @@ def send_result(host_, data):
 
         un = 'mec'
         pw = 'password'
-        port = 22
+        s_port = 22
 
         c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        c.connect(host_, port, un, pw)
+        c.connect(host_, s_port, un, pw)
         for i in data:
             cmd = ('echo "{}" >> /home/mec/result/data.py'.format(i))  # task share : host ip task
             stdin, stdout, stderr = c.exec_command(cmd)
