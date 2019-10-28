e5ea8ef9ed8d39b4b3174a582df7973834e40945
Emeka Ugwuanyi <emylincon@gmail.com>
2019-10-28 20:12:36 +0000

    3d plots rendering

---
+++ 2algo/results and data complilation/3d_scatter_plot.py
@@ -0,0 +1,22 @@
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+# x, y, z = axes3d.get_test_data(0.05)
+
+x = [1, 2, 3, 5,  1, 8, 9]
+y = [1, 7, 5, 9, 1, 2, 3]
+z = [1, 6, 8, 4, 5, 8, 3]
+
+#ax.scatter(x,y,z, c='r', marker='o')
+ax.scatter(x,y,z, c=np.linalg.norm([x,y,x], axis=0), marker='o')
+
+
+ax.set_ylabel('y axis')
+ax.set_xlabel('x axis')
+ax.set_zlabel('z axis')
+
+plt.show()
