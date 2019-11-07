ce5d4d117fdb52cb74440637f5a1feb0b5b0e45d
Emeka Ugwuanyi <emylincon@gmail.com>
2019-11-07 09:57:06 +0000

    added dynamic mec numbering while saving experimental data

--- algo/9_rms_wait_die_gui.py
+++ algo/9_rms_wait_die_gui.py
@@ -634,13 +634,21 @@ def start_loop():
             print('\nEnter "Exit" to stop Programme!')
         if x == 'exit':
             print('\nProgramme Terminated')
-            cmd = 'echo "wt_8_6 = {} \nrtt_8_6 = {} \ncpu_8_6 = {} \noff_mec8_6 = {}' \
-                  '\noff_cloud8_6 = {} \nloc8_6 = {}" >> data.py'.format(mec_waiting_time,
-                                                                     mec_rtt,
-                                                                     _cpu,
-                                                                     _off_mec,
-                                                                     _off_cloud,
-                                                                     _loc)
+            cmd = 'echo "wt_8_{} = {} \nrtt_8_{} = {} \ncpu_8_{} = {} ' \
+                  '\noff_mec8_{} = {}' \
+                  '\noff_cloud8_{} = {} \nloc8_{} = {}" >> data.py'.format(mec_no,
+                                                                           mec_waiting_time,
+                                                                           mec_no,
+                                                                           mec_rtt,
+                                                                           mec_no,
+                                                                           _cpu,
+                                                                           mec_no,
+                                                                           _off_mec,
+                                                                           mec_no,
+                                                                           _off_cloud,
+                                                                           mec_no,
+                                                                           _loc
+                                                                           )
             os.system(cmd)
             break
 
