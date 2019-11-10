08be16cc8aeffc005f9f11486c16f544a0d52106
Emeka Ugwuanyi <emylincon@gmail.com>
2019-11-10 22:25:53 +0000

    added 7 mec plot for homogeneous data

---
+++ algo/Experiment_data/7cpu_compare.py
@@ -0,0 +1,106 @@
+from drawnow import *
+from matplotlib import pyplot as plt
+import data
+import cpu_redo as cp
+import cpu6_redo as cp6
+import redo_data as rd
+
+fig = plt.figure()
+ax1 = fig.add_subplot(141)
+ax2 = fig.add_subplot(142)
+ax3 = fig.add_subplot(143)
+ax4 = fig.add_subplot(144)
+
+
+def _mov_avg(a1):
+    ma1=[] # moving average list
+    avg1=0 # movinf average pointwise
+    count=0
+    for i in range(len(a1)):
+        count+=1
+        avg1=((count-1)*avg1+a1[i])/count
+        ma1.append(avg1) #cumulative average formula
+        # μ_n=((n-1) μ_(n-1)  + x_n)/n
+    return ma1
+
+
+def four_mec():
+    ax1.grid(True)
+    ax1.plot(list(range(500)), _mov_avg(data.cpu_1), linewidth=2, label='RMS + Bankers')
+    ax1.plot(list(range(500)), _mov_avg(data.cpu_3), linewidth=2, label='EDF + Bankers')
+    ax1.plot(list(range(500)), _mov_avg(data.cpu_5), linewidth=2, label='RMS + wound wait')
+    ax1.plot(list(range(500)), _mov_avg(data.cpu_8), linewidth=2, label='RMS + wait die')
+    ax1.plot(list(range(500)), _mov_avg(data.cpu_11), linewidth=2, label='EDF + wound wait')
+    ax1.plot(list(range(500)), _mov_avg(data.cpu_16), linewidth=2, label='EDF + wait die')
+
+    ax1.set_title('Moving CPU Utilization for 4 MEC Set-up')
+    ax1.set_ylabel('Moving CPU')
+    ax1.set_xlabel('Time (seconds)')
+    ax1.legend()
+    plt.subplot(ax1)
+
+
+def five_mec():
+    ax2.grid(True)
+    #ax2.plot(list(range(500)), _mov_avg(data.cpu_1_5), linewidth=2, label='RMS + Bankers')
+    ax2.plot(list(range(500)), _mov_avg(cp.cpu_1_5), linewidth=2, label='RMS + Bankers')
+    ax2.plot(list(range(500)), _mov_avg(data.cpu_3_5), linewidth=2, label='EDF + Bankers')
+    ax2.plot(list(range(500)), _mov_avg(data.cpu_5_5), linewidth=2, label='RMS + wound wait')
+    ax2.plot(list(range(500)), _mov_avg(data.cpu_8_5), linewidth=2, label='RMS + wait die')
+    ax2.plot(list(range(500)), _mov_avg(data.cpu_11_5), linewidth=2, label='EDF + wound wait')
+    ax2.plot(list(range(500)), _mov_avg(data.cpu_16_5), linewidth=2, label='EDF + wait die')
+
+    ax2.set_title('Moving CPU Utilization for 5 MEC Set-up')
+    ax2.set_ylabel('Moving CPU')
+    ax2.set_xlabel('Time (seconds)')
+    ax2.legend()
+    plt.subplot(ax2)
+
+
+def six_mec():
+    ax3.grid(True)
+    #ax3.plot(list(range(500)), _mov_avg(data.cpu_1_6), linewidth=2, label='RMS + Bankers')
+    ax3.plot(list(range(500)), _mov_avg(cp6.cpu_1_6), linewidth=2, label='RMS + Bankers')
+    ax3.plot(list(range(500)), _mov_avg(data.cpu_3_6), linewidth=2, label='EDF + Bankers')
+    ax3.plot(list(range(500)), _mov_avg(data.cpu_5_6), linewidth=2, label='RMS + wound wait')
+    ax3.plot(list(range(500)), _mov_avg(data.cpu_8_6), linewidth=2, label='RMS + wait die')
+    ax3.plot(list(range(500)), _mov_avg(data.cpu_11_6), linewidth=2, label='EDF + wound wait')
+    ax3.plot(list(range(500)), _mov_avg(data.cpu_16_6), linewidth=2, label='EDF + wait die')
+
+    ax3.set_title('Moving CPU Utilization for 6 MEC Set-up')
+    ax3.set_ylabel('Moving CPU')
+    ax3.set_xlabel('Time (seconds)')
+    ax3.legend()
+    plt.subplot(ax3)
+
+
+def seven_mec():
+    ax4.grid(True)
+    ax4.plot(list(range(500)), _mov_avg(rd.cpu_1_7), linewidth=2, label='RMS + Bankers')
+    ax4.plot(list(range(500)), _mov_avg(rd.cpu_3_7), linewidth=2, label='EDF + Bankers')
+    ax4.plot(list(range(500)), _mov_avg(rd.cpu_5_7), linewidth=2, label='RMS + wound wait')
+    ax4.plot(list(range(500)), _mov_avg(rd.cpu_8_7), linewidth=2, label='RMS + wait die')
+    ax4.plot(list(range(500)), _mov_avg(rd.cpu_11_7), linewidth=2, label='EDF + wound wait')
+    ax4.plot(list(range(500)), _mov_avg(rd.cpu_16_7), linewidth=2, label='EDF + wait die')
+
+    ax4.set_title('Moving CPU Utilization for 7 MEC Set-up')
+    ax4.set_ylabel('Moving CPU')
+    ax4.set_xlabel('Time (seconds)')
+    ax4.legend()
+    plt.subplot(ax4)
+
+
+def plot_graphs():
+    four_mec()
+    five_mec()
+    six_mec()
+    seven_mec()
+    fig.suptitle('MEC CPU Utilization During Deadlock Experiment')
+    plt.show()
+
+
+def show_graphs():
+    drawnow(plot_graphs)
+
+
+show_graphs()
