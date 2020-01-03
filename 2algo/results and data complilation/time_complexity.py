6bcf01e3e310286c2f22f2b8228a0eb3c65d6547
Emeka Ugwuanyi <emylincon@gmail.com>
2020-01-03 21:21:04 +0000

    added code names

--- 2algo/results and data complilation/time_complexity.py
+++ 2algo/results and data complilation/time_complexity.py
@@ -1,12 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+plt.rcParams.update({'font.size': 22})
 # marker size
 n = 10
 
-x = np.arange(1,100,10)
-#x = np.arange(1, 7)
-
+x = np.arange(1, 100, 10)
+# x = np.arange(1, 7)
+algo_dict = {'RMS+Bankers': r'$ALG_1$',
+             'EDF+Bankers': r'$ALG_2$',
+             'RMS+wound wait': r'$ALG_3$',
+             'RMS+wait die': r'$ALG_4$',
+             'EDF+wound wait': r'$ALG_5$',
+             'EDF+wait die': r'$ALG_6$'}
 y1 = x ** 2
 y2 = x * (2 ** (1 / x) - 1)
 y3 = x * np.log2(x)
@@ -24,12 +31,12 @@ plt.grid(True)
 
 plt.yscale('log')
 
-plt.plot(x, case1, 'r--+', label='RMS+Bankers', markersize=n)
-plt.plot(x, case2, 'g-->', label='EDF+Bankers', markersize=n)
-plt.plot(x, case3, 'y--o', label='RMS+Wound-wait', markersize=n)
-plt.plot(x, case4, 'b--*', label='RMS+wait-die', markersize=n)
-plt.plot(x, case5, 'c--s', label='EDF+Wound-wait', markersize=n)
-plt.plot(x, case6, 'k--^', label='EDF+Wait-die', markersize=n)
+plt.plot(x, case1, 'r--+', label=algo_dict['RMS+Bankers'], markersize=n)
+plt.plot(x, case2, 'g-->', label=algo_dict['EDF+Bankers'], markersize=n)
+plt.plot(x, case3, 'y--o', label=algo_dict['RMS+wound wait'], markersize=n)
+plt.plot(x, case4, 'b--*', label=algo_dict['RMS+wait die'], markersize=n)
+plt.plot(x, case5, 'c--s', label=algo_dict['EDF+wound wait'], markersize=n)
+plt.plot(x, case6, 'k--^', label=algo_dict['EDF+wait die'], markersize=n)
 plt.ylabel('No of Process')
 plt.xlabel('No of Resources')
 
@@ -39,3 +46,5 @@ plt.title('Time Complexity Analysis')
 plt.legend()
 # ax_rt.legend()
 plt.show()
+
+#https://matplotlib.org/3.1.1/tutorials/text/mathtext.html
