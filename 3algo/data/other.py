f3d356b57d3596d80274fa38c4d619fb59827cf7
Emeka Ugwuanyi <emylincon@gmail.com>
2020-02-20 12:01:04 +0000

    added experimental result data

---
+++ 3algo/data/other.py
@@ -0,0 +1,904 @@
+off_mec6_16_7 = 198
+off_cloud6_16_7 = 232
+inward_mec6_16_7 = 157
+loc6_16_7 = 1878
+deadlock6_16_7 = [14]
+
+off_mec0_16_4 = 100
+off_cloud0_16_4 = 163
+inward_mec0_16_4 = 122
+loc0_16_4 = 1349
+deadlock0_16_4 = [9]
+
+off_mec0_16_5 = 158
+off_cloud0_16_5 = 551
+inward_mec0_16_5 = 113
+loc0_16_5 = 2815
+deadlock0_16_5 = [21]
+
+off_mec0_16_6 = 137
+off_cloud0_16_6 = 85
+inward_mec0_16_6 = 141
+loc0_16_6 = 945
+deadlock0_16_6 = [9]
+
+off_mec0_16_7 = 192
+off_cloud0_16_7 = 238
+inward_mec0_16_7 = 123
+loc0_16_7 = 2024
+deadlock0_16_7 = [13]
+
+off_mec3_16_4 = 131
+off_cloud3_16_4 = 546
+inward_mec3_16_4 = 113
+loc3_16_4 = 3284
+deadlock3_16_4 = [18]
+
+off_mec3_16_5 = 158
+off_cloud3_16_5 = 304
+inward_mec3_16_5 = 137
+loc3_16_5 = 2400
+deadlock3_16_5 = [17]
+
+off_mec3_16_6 = 168
+off_cloud3_16_6 = 253
+inward_mec3_16_6 = 138
+loc3_16_6 = 2251
+deadlock3_16_6 = [14]
+
+off_mec3_16_7 = 163
+off_cloud3_16_7 = 146
+inward_mec3_16_7 = 175
+loc3_16_7 = 1314
+deadlock3_16_7 = [13]
+
+off_mec4_16_5 = 161
+off_cloud4_16_5 = 393
+inward_mec4_16_5 = 132
+loc4_16_5 = 2474
+deadlock4_16_5 = [17]
+
+off_mec4_16_6 = 153
+off_cloud4_16_6 = 221
+inward_mec4_16_6 = 149
+loc4_16_6 = 1948
+deadlock4_16_6 = [15]
+
+off_mec4_16_7 = 214
+off_cloud4_16_7 = 168
+inward_mec4_16_7 = 152
+loc4_16_7 = 1446
+deadlock4_16_7 = [4]
+
+off_mec5_16_6 = 157
+off_cloud5_16_6 = 246
+inward_mec5_16_6 = 144
+loc5_16_6 = 1791
+deadlock5_16_6 = [10]
+
+off_mec5_16_7 = 194
+off_cloud5_16_7 = 214
+inward_mec5_16_7 = 163
+loc5_16_7 = 1977
+deadlock5_16_7 = [13]
+
+off_mec1_16_4 = 117
+off_cloud1_16_4 = 286
+inward_mec1_16_4 = 128
+loc1_16_4 = 1872
+deadlock1_16_4 = [20]
+
+off_mec1_16_5 = 118
+off_cloud1_16_5 = 127
+inward_mec1_16_5 = 158
+loc1_16_5 = 1226
+deadlock1_16_5 = [8]
+
+off_mec1_16_6 = 103
+off_cloud1_16_6 = 103
+inward_mec1_16_6 = 168
+loc1_16_6 = 917
+deadlock1_16_6 = [10]
+
+off_mec1_16_7 = 129
+off_cloud1_16_7 = 18
+inward_mec1_16_7 = 179
+loc1_16_7 = 1009
+deadlock1_16_7 = [4]
+
+off_mec2_16_4 = 126
+off_cloud2_16_4 = 604
+inward_mec2_16_4 = 111
+loc2_16_4 = 3503
+deadlock2_16_4 = [31]
+
+off_mec2_16_5 = 103
+off_cloud2_16_5 = 115
+inward_mec2_16_5 = 158
+loc2_16_5 = 1209
+deadlock2_16_5 = [13]
+
+off_mec2_16_6 = 161
+off_cloud2_16_6 = 268
+inward_mec2_16_6 = 139
+loc2_16_6 = 2082
+deadlock2_16_6 = [18]
+
+off_mec2_16_7 = 49
+off_cloud2_16_7 = 19
+inward_mec2_16_7 = 190
+loc2_16_7 = 518
+deadlock2_16_7 = [5]
+
+off_mec6_12_7 = 199
+off_cloud6_12_7 = 231
+inward_mec6_12_7 = 148
+loc6_12_7 = 1878
+deadlock6_12_7 = [14]
+
+off_mec0_12_4 = 101
+off_cloud0_12_4 = 162
+inward_mec0_12_4 = 125
+loc0_12_4 = 1349
+deadlock0_12_4 = [9]
+
+off_mec3_12_4 = 134
+off_cloud3_12_4 = 543
+inward_mec3_12_4 = 114
+loc3_12_4 = 3284
+deadlock3_12_4 = [18]
+
+off_mec4_12_5 = 161
+off_cloud4_12_5 = 393
+inward_mec4_12_5 = 128
+loc4_12_5 = 2474
+deadlock4_12_5 = [17]
+
+off_mec4_12_6 = 153
+off_cloud4_12_6 = 221
+inward_mec4_12_6 = 144
+loc4_12_6 = 1948
+deadlock4_12_6 = [15]
+
+off_mec4_12_7 = 215
+off_cloud4_12_7 = 167
+inward_mec4_12_7 = 157
+loc4_12_7 = 1446
+deadlock4_12_7 = [4]
+
+off_mec5_12_6 = 158
+off_cloud5_12_6 = 245
+inward_mec5_12_6 = 147
+loc5_12_6 = 1791
+deadlock5_12_6 = [10]
+
+off_mec5_12_7 = 195
+off_cloud5_12_7 = 213
+inward_mec5_12_7 = 157
+loc5_12_7 = 1977
+deadlock5_12_7 = [13]
+
+off_mec1_12_4 = 118
+off_cloud1_12_4 = 285
+inward_mec1_12_4 = 129
+loc1_12_4 = 1872
+deadlock1_12_4 = [20]
+
+off_mec1_12_5 = 117
+off_cloud1_12_5 = 128
+inward_mec1_12_5 = 159
+loc1_12_5 = 1226
+deadlock1_12_5 = [8]
+
+off_mec1_12_6 = 103
+off_cloud1_12_6 = 103
+inward_mec1_12_6 = 171
+loc1_12_6 = 917
+deadlock1_12_6 = [10]
+
+off_mec1_12_7 = 129
+off_cloud1_12_7 = 18
+inward_mec1_12_7 = 183
+loc1_12_7 = 1009
+deadlock1_12_7 = [4]
+
+off_mec2_12_4 = 127
+off_cloud2_12_4 = 603
+inward_mec2_12_4 = 112
+loc2_12_4 = 3503
+deadlock2_12_4 = [31]
+
+off_mec2_12_5 = 102
+off_cloud2_12_5 = 116
+inward_mec2_12_5 = 165
+loc2_12_5 = 1209
+deadlock2_12_5 = [13]
+
+off_mec2_12_6 = 161
+off_cloud2_12_6 = 268
+inward_mec2_12_6 = 140
+loc2_12_6 = 2082
+deadlock2_12_6 = [18]
+
+off_mec2_12_7 = 49
+off_cloud2_12_7 = 19
+inward_mec2_12_7 = 205
+loc2_12_7 = 518
+deadlock2_12_7 = [5]
+
+off_mec6_10_7 = 211
+off_cloud6_10_7 = 326
+inward_mec6_10_7 = 172
+loc6_10_7 = 1889
+deadlock6_10_7 = [14]
+
+off_mec0_10_4 = 107
+off_cloud0_10_4 = 233
+inward_mec0_10_4 = 135
+loc0_10_4 = 1358
+deadlock0_10_4 = [9]
+
+off_mec0_10_5 = 152
+off_cloud0_10_5 = 711
+inward_mec0_10_5 = 117
+loc0_10_5 = 2827
+deadlock0_10_5 = [21]
+
+off_mec0_10_6 = 146
+off_cloud0_10_6 = 123
+inward_mec0_10_6 = 148
+loc0_10_6 = 954
+deadlock0_10_6 = [9]
+
+off_mec0_10_7 = 210
+off_cloud0_10_7 = 321
+inward_mec0_10_7 = 141
+loc0_10_7 = 2029
+deadlock0_10_7 = [13]
+
+off_mec3_10_4 = 141
+off_cloud3_10_4 = 693
+inward_mec3_10_4 = 120
+loc3_10_4 = 3285
+deadlock3_10_4 = [18]
+
+off_mec3_10_5 = 149
+off_cloud3_10_5 = 416
+inward_mec3_10_5 = 133
+loc3_10_5 = 2397
+deadlock3_10_5 = [17]
+
+off_mec3_10_6 = 183
+off_cloud3_10_6 = 346
+inward_mec3_10_6 = 150
+loc3_10_6 = 2267
+deadlock3_10_6 = [14]
+
+off_mec3_10_7 = 174
+off_cloud3_10_7 = 205
+inward_mec3_10_7 = 200
+loc3_10_7 = 1320
+deadlock3_10_7 = [13]
+
+off_mec4_10_5 = 158
+off_cloud4_10_5 = 545
+inward_mec4_10_5 = 130
+loc4_10_5 = 2475
+deadlock4_10_5 = [17]
+
+off_mec4_10_6 = 166
+off_cloud4_10_6 = 293
+inward_mec4_10_6 = 158
+loc4_10_6 = 1959
+deadlock4_10_6 = [15]
+
+off_mec4_10_7 = 236
+off_cloud4_10_7 = 237
+inward_mec4_10_7 = 172
+loc4_10_7 = 1445
+deadlock4_10_7 = [4]
+
+off_mec5_10_6 = 172
+off_cloud5_10_6 = 315
+inward_mec5_10_6 = 159
+loc5_10_6 = 1807
+deadlock5_10_6 = [10]
+
+off_mec5_10_7 = 220
+off_cloud5_10_7 = 291
+inward_mec5_10_7 = 176
+loc5_10_7 = 1982
+deadlock5_10_7 = [13]
+
+off_mec1_10_4 = 123
+off_cloud1_10_4 = 366
+inward_mec1_10_4 = 138
+loc1_10_4 = 1894
+deadlock1_10_4 = [20]
+
+off_mec1_10_5 = 118
+off_cloud1_10_5 = 184
+inward_mec1_10_5 = 150
+loc1_10_5 = 1227
+deadlock1_10_5 = [8]
+
+off_mec1_10_6 = 114
+off_cloud1_10_6 = 146
+inward_mec1_10_6 = 186
+loc1_10_6 = 917
+deadlock1_10_6 = [10]
+
+off_mec1_10_7 = 165
+off_cloud1_10_7 = 18
+inward_mec1_10_7 = 208
+loc1_10_7 = 1015
+deadlock1_10_7 = [4]
+
+off_mec2_10_4 = 137
+off_cloud2_10_4 = 772
+inward_mec2_10_4 = 115
+loc2_10_4 = 3504
+deadlock2_10_4 = [31]
+
+off_mec2_10_5 = 108
+off_cloud2_10_5 = 168
+inward_mec2_10_5 = 155
+loc2_10_5 = 1203
+deadlock2_10_5 = [13]
+
+off_mec2_10_6 = 173
+off_cloud2_10_6 = 368
+inward_mec2_10_6 = 153
+loc2_10_6 = 2098
+deadlock2_10_6 = [18]
+
+off_mec2_10_7 = 74
+off_cloud2_10_7 = 19
+inward_mec2_10_7 = 221
+loc2_10_7 = 519
+deadlock2_10_7 = [5]
+
+off_mec6_7_7 = 213
+off_cloud6_7_7 = 324
+inward_mec6_7_7 = 182
+loc6_7_7 = 1889
+deadlock6_7_7 = [14]
+
+off_mec0_7_4 = 110
+off_cloud0_7_4 = 230
+inward_mec0_7_4 = 134
+loc0_7_4 = 1358
+deadlock0_7_4 = [9]
+
+off_mec0_7_5 = 165
+off_cloud0_7_5 = 698
+inward_mec0_7_5 = 124
+loc0_7_5 = 2827
+deadlock0_7_5 = [21]
+
+off_mec0_7_6 = 144
+off_cloud0_7_6 = 125
+inward_mec0_7_6 = 154
+loc0_7_6 = 954
+deadlock0_7_6 = [9]
+
+off_mec0_7_7 = 209
+off_cloud0_7_7 = 322
+inward_mec0_7_7 = 145
+loc0_7_7 = 2029
+deadlock0_7_7 = [13]
+
+off_mec3_7_4 = 141
+off_cloud3_7_4 = 693
+inward_mec3_7_4 = 121
+loc3_7_4 = 3285
+deadlock3_7_4 = [18]
+
+off_mec3_7_5 = 170
+off_cloud3_7_5 = 395
+inward_mec3_7_5 = 150
+loc3_7_5 = 2397
+deadlock3_7_5 = [17]
+
+off_mec3_7_6 = 183
+off_cloud3_7_6 = 346
+inward_mec3_7_6 = 151
+loc3_7_6 = 2267
+deadlock3_7_6 = [14]
+
+off_mec3_7_7 = 176
+off_cloud3_7_7 = 203
+inward_mec3_7_7 = 188
+loc3_7_7 = 1320
+deadlock3_7_7 = [13]
+
+off_mec4_7_5 = 180
+off_cloud4_7_5 = 523
+inward_mec4_7_5 = 137
+loc4_7_5 = 2475
+deadlock4_7_5 = [17]
+
+off_mec4_7_6 = 167
+off_cloud4_7_6 = 292
+inward_mec4_7_6 = 157
+loc4_7_6 = 1959
+deadlock4_7_6 = [15]
+
+off_mec4_7_7 = 238
+off_cloud4_7_7 = 235
+inward_mec4_7_7 = 172
+loc4_7_7 = 1445
+deadlock4_7_7 = [4]
+
+off_mec5_7_6 = 173
+off_cloud5_7_6 = 314
+inward_mec5_7_6 = 158
+loc5_7_6 = 1807
+deadlock5_7_6 = [10]
+
+off_mec5_7_7 = 222
+off_cloud5_7_7 = 289
+inward_mec5_7_7 = 175
+loc5_7_7 = 1982
+deadlock5_7_7 = [13]
+
+off_mec1_7_4 = 121
+off_cloud1_7_4 = 368
+inward_mec1_7_4 = 140
+loc1_7_4 = 1894
+deadlock1_7_4 = [20]
+
+off_mec1_7_6 = 113
+off_cloud1_7_6 = 147
+inward_mec1_7_6 = 182
+loc1_7_6 = 917
+deadlock1_7_6 = [10]
+
+off_mec1_7_5 = 125
+off_cloud1_7_5 = 177
+inward_mec1_7_5 = 171
+loc1_7_5 = 1227
+deadlock1_7_5 = [8]
+
+off_mec1_7_7 = 165
+off_cloud1_7_7 = 18
+inward_mec1_7_7 = 208
+loc1_7_7 = 1015
+deadlock1_7_7 = [4]
+
+off_mec2_7_4 = 136
+off_cloud2_7_4 = 773
+inward_mec2_7_4 = 113
+loc2_7_4 = 3504
+deadlock2_7_4 = [31]
+
+off_mec2_7_5 = 112
+off_cloud2_7_5 = 164
+inward_mec2_7_5 = 170
+loc2_7_5 = 1203
+deadlock2_7_5 = [13]
+
+off_mec2_7_6 = 173
+off_cloud2_7_6 = 368
+inward_mec2_7_6 = 151
+loc2_7_6 = 2098
+deadlock2_7_6 = [18]
+
+off_mec2_7_7 = 74
+off_cloud2_7_7 = 19
+inward_mec2_7_7 = 227
+loc2_7_7 = 519
+deadlock2_7_7 = [5]
+
+off_mec0_3_4 = 103
+off_cloud0_3_4 = 160
+inward_mec0_3_4 = 124
+loc0_3_4 = 1349
+deadlock0_3_4 = [9]
+
+off_mec0_3_5 = 153
+off_cloud0_3_5 = 556
+inward_mec0_3_5 = 108
+loc0_3_5 = 2815
+deadlock0_3_5 = [21]
+
+off_mec0_3_6 = 137
+off_cloud0_3_6 = 85
+inward_mec0_3_6 = 134
+loc0_3_6 = 945
+deadlock0_3_6 = [9]
+
+off_mec0_3_7 = 190
+off_cloud0_3_7 = 240
+inward_mec0_3_7 = 133
+loc0_3_7 = 2024
+deadlock0_3_7 = [13]
+
+off_mec3_3_4 = 133
+off_cloud3_3_4 = 544
+inward_mec3_3_4 = 113
+loc3_3_4 = 3284
+deadlock3_3_4 = [18]
+
+off_mec3_3_5 = 156
+off_cloud3_3_5 = 306
+inward_mec3_3_5 = 131
+loc3_3_5 = 2400
+deadlock3_3_5 = [17]
+
+off_mec3_3_6 = 166
+off_cloud3_3_6 = 255
+inward_mec3_3_6 = 140
+loc3_3_6 = 2251
+deadlock3_3_6 = [14]
+
+off_mec3_3_7 = 164
+off_cloud3_3_7 = 145
+inward_mec3_3_7 = 169
+loc3_3_7 = 1314
+deadlock3_3_7 = [13]
+
+off_mec4_3_5 = 155
+off_cloud4_3_5 = 399
+inward_mec4_3_5 = 129
+loc4_3_5 = 2474
+deadlock4_3_5 = [17]
+
+off_mec4_3_6 = 150
+off_cloud4_3_6 = 224
+inward_mec4_3_6 = 141
+loc4_3_6 = 1948
+deadlock4_3_6 = [15]
+
+off_mec4_3_7 = 216
+off_cloud4_3_7 = 166
+inward_mec4_3_7 = 153
+loc4_3_7 = 1446
+deadlock4_3_7 = [4]
+
+off_mec5_3_6 = 157
+off_cloud5_3_6 = 246
+inward_mec5_3_6 = 147
+loc5_3_6 = 1791
+deadlock5_3_6 = [10]
+
+off_mec5_3_7 = 192
+off_cloud5_3_7 = 216
+inward_mec5_3_7 = 159
+loc5_3_7 = 1977
+deadlock5_3_7 = [13]
+
+off_mec1_3_4 = 118
+off_cloud1_3_4 = 285
+inward_mec1_3_4 = 125
+loc1_3_4 = 1872
+deadlock1_3_4 = [20]
+
+off_mec1_3_5 = 107
+off_cloud1_3_5 = 138
+inward_mec1_3_5 = 153
+loc1_3_5 = 1226
+deadlock1_3_5 = [8]
+
+off_mec1_3_6 = 103
+off_cloud1_3_6 = 103
+inward_mec1_3_6 = 170
+loc1_3_6 = 917
+deadlock1_3_6 = [10]
+
+off_mec1_3_7 = 129
+off_cloud1_3_7 = 18
+inward_mec1_3_7 = 172
+loc1_3_7 = 1009
+deadlock1_3_7 = [4]
+
+off_mec2_3_4 = 124
+off_cloud2_3_4 = 606
+inward_mec2_3_4 = 116
+loc2_3_4 = 3503
+deadlock2_3_4 = [31]
+
+off_mec2_3_5 = 102
+off_cloud2_3_5 = 116
+inward_mec2_3_5 = 152
+loc2_3_5 = 1209
+deadlock2_3_5 = [13]
+
+off_mec2_3_6 = 161
+off_cloud2_3_6 = 268
+inward_mec2_3_6 = 142
+loc2_3_6 = 2082
+deadlock2_3_6 = [18]
+
+off_mec2_3_7 = 49
+off_cloud2_3_7 = 19
+inward_mec2_3_7 = 197
+loc2_3_7 = 518
+deadlock2_3_7 = [5]
+
+off_mec6_2_7 = 145
+off_cloud6_2_7 = 675
+inward_mec6_2_7 = 123
+loc6_2_7 = 1606
+deadlock6_2_7 = [14]
+
+off_mec0_2_5 = 132
+off_cloud0_2_5 = 1192
+inward_mec0_2_5 = 96
+loc0_2_5 = 2366
+deadlock0_2_5 = [21]
+
+off_mec0_2_6 = 124
+off_cloud0_2_6 = 244
+inward_mec0_2_6 = 121
+loc0_2_6 = 855
+deadlock0_2_6 = [9]
+
+off_mec0_2_7 = 160
+off_cloud0_2_7 = 644
+inward_mec0_2_7 = 107
+loc0_2_7 = 1756
+deadlock0_2_7 = [13]
+
+off_mec3_2_5 = 119
+off_cloud3_2_5 = 825
+inward_mec3_2_5 = 96
+loc3_2_5 = 2018
+deadlock3_2_5 = [17]
+
+off_mec3_2_6 = 133
+off_cloud3_2_6 = 791
+inward_mec3_2_6 = 103
+loc3_2_6 = 1872
+deadlock3_2_6 = [14]
+
+off_mec3_2_7 = 112
+off_cloud3_2_7 = 448
+inward_mec3_2_7 = 138
+loc3_2_7 = 1139
+deadlock3_2_7 = [13]
+
+off_mec5_2_6 = 118
+off_cloud5_2_6 = 650
+inward_mec5_2_6 = 113
+loc5_2_6 = 1526
+deadlock5_2_6 = [10]
+
+off_mec5_2_7 = 157
+off_cloud5_2_7 = 670
+inward_mec5_2_7 = 126
+loc5_2_7 = 1666
+deadlock5_2_7 = [13]
+
+off_mec4_2_5 = 121
+off_cloud4_2_5 = 999
+inward_mec4_2_5 = 101
+loc4_2_5 = 2058
+deadlock4_2_5 = [17]
+
+off_mec4_2_6 = 113
+off_cloud4_2_6 = 692
+inward_mec4_2_6 = 103
+loc4_2_6 = 1613
+deadlock4_2_6 = [15]
+
+off_mec4_2_7 = 147
+off_cloud4_2_7 = 544
+inward_mec4_2_7 = 113
+loc4_2_7 = 1227
+deadlock4_2_7 = [4]
+
+off_mec1_2_5 = 88
+off_cloud1_2_5 = 375
+inward_mec1_2_5 = 130
+loc1_2_5 = 1066
+deadlock1_2_5 = [8]
+
+off_mec1_2_6 = 78
+off_cloud1_2_6 = 340
+inward_mec1_2_6 = 137
+loc1_2_6 = 759
+deadlock1_2_6 = [10]
+
+off_mec1_2_7 = 127
+off_cloud1_2_7 = 199
+inward_mec1_2_7 = 146
+loc1_2_7 = 872
+deadlock1_2_7 = [4]
+
+off_mec2_2_5 = 89
+off_cloud2_2_5 = 365
+inward_mec2_2_5 = 126
+loc2_2_5 = 1025
+deadlock2_2_5 = [13]
+
+off_mec2_2_6 = 117
+off_cloud2_2_6 = 804
+inward_mec2_2_6 = 106
+loc2_2_6 = 1718
+deadlock2_2_6 = [18]
+
+off_mec2_2_7 = 60
+off_cloud2_2_7 = 100
+inward_mec2_2_7 = 155
+loc2_2_7 = 452
+deadlock2_2_7 = [5]
+
+off_mec0_2_4 = 83
+off_cloud0_2_4 = 407
+inward_mec0_2_4 = 105
+loc0_2_4 = 1208
+deadlock0_2_4 = [9]
+
+off_mec0_2_5 = 112
+off_cloud0_2_5 = 1118
+inward_mec0_2_5 = 95
+loc0_2_5 = 2236
+deadlock0_2_5 = [23]
+
+off_mec2_2_4 = 103
+off_cloud2_2_4 = 1516
+inward_mec2_2_4 = 89
+loc2_2_4 = 2794
+deadlock2_2_4 = [31]
+
+off_mec3_2_4 = 101
+off_cloud3_2_4 = 1355
+inward_mec3_2_4 = 85
+loc3_2_4 = 2663
+deadlock3_2_4 = [18]
+
+off_mec1_2_4 = 93
+off_cloud1_2_4 = 690
+inward_mec1_2_4 = 101
+loc1_2_4 = 1600
+deadlock1_2_4 = [20]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 3algo/data/timely.py
@@ -0,0 +1,308 @@
+# EDF + Wait_die
+
+timely5_16_4 = 3191
+untimely5_16_4 = 286
+
+timely5_16_5 = 3473
+untimely5_16_5 = 162
+
+timely5_16_6 = 3550
+untimely5_16_6 = 182
+
+timely5_16_7 = 3542
+untimely5_16_7 = 213
+
+timely4_16_4 = 3159
+untimely4_16_4 = 429
+
+timely4_16_5 = 3494
+untimely4_16_5 = 101
+
+timely4_16_6 = 3471
+untimely4_16_6 = 116
+
+timely4_16_7 = 3654
+untimely4_16_7 = 169
+
+timely6_16_4 = 3106
+untimely6_16_4 = 403
+
+timely6_16_5 = 3523
+untimely6_16_5 = 177
+
+timely6_16_6 = 3506
+untimely6_16_6 = 116
+
+timely6_16_7 = 3667
+untimely6_16_7 = 239
+
+timely5_12_4 = 2558
+untimely5_12_4 = 914
+
+timely5_12_5 = 3569
+untimely5_12_5 = 74
+
+timely5_12_6 = 3702
+untimely5_12_6 = 24
+
+timely5_12_7 = 3675
+untimely5_12_7 = 72
+
+timely6_12_4 = 2615
+untimely6_12_4 = 885
+
+timely6_12_5 = 3603
+untimely6_12_5 = 80
+
+timely6_12_6 = 3556
+untimely6_12_6 = 66
+
+timely6_12_7 = 3844
+untimely6_12_7 = 62
+
+timely4_12_4 = 2629
+untimely4_12_4 = 973
+
+timely4_12_5 = 3479
+untimely4_12_5 = 115
+
+timely4_12_6 = 3515
+untimely4_12_6 = 89
+
+timely4_12_7 = 3749
+untimely4_12_7 = 47
+
+timely4_10_4 = 2907
+untimely4_10_4 = 718
+
+
+timely4_10_5 = 3119
+untimely4_10_5 = 486
+
+timely4_10_6 = 3116
+untimely4_10_6 = 555
+
+timely4_10_7 = 3387
+untimely4_10_7 = 502
+
+timely6_10_4 = 2718
+untimely6_10_4 = 779
+
+timely6_10_5 = 3143
+untimely6_10_5 = 565
+
+timely6_10_6 = 3073
+untimely6_10_6 = 578
+
+timely6_10_7 = 3337
+untimely6_10_7 = 649
+
+timely5_10_4 = 2863
+untimely5_10_4 = 653
+
+timely5_10_5 = 3102
+untimely5_10_5 = 495
+
+timely5_10_6 = 3166
+untimely5_10_6 = 607
+
+timely5_10_7 = 3228
+untimely5_10_7 = 589
+
+timely4_7_4 = 3144
+untimely4_7_4 = 481
+
+timely4_7_5 = 3351
+untimely4_7_5 = 282
+
+timely4_7_6 = 3378
+untimely4_7_6 = 294
+
+timely4_7_7 = 3588
+untimely4_7_7 = 304
+
+timely6_7_4 = 3088
+untimely6_7_4 = 415
+
+timely6_7_5 = 3322
+untimely6_7_5 = 395
+
+timely6_7_6 = 3354
+untimely6_7_6 = 289
+
+timely6_7_7 = 3646
+untimely6_7_7 = 361
+
+timely5_7_4 = 3064
+untimely5_7_4 = 450
+
+timely5_7_5 = 3411
+untimely5_7_5 = 251
+
+timely5_7_6 = 3373
+untimely5_7_6 = 403
+
+timely5_7_7 = 3438
+untimely5_7_7 = 377
+
+timely6_3_4 = 3321
+untimely6_3_4 = 186
+
+timely6_3_5 = 3483
+untimely6_3_5 = 189
+
+timely6_3_6 = 3470
+untimely6_3_6 = 154
+
+timely6_3_7 = 3787
+untimely6_3_7 = 125
+
+timely4_3_4 = 3338
+untimely4_3_4 = 250
+
+timely4_3_5 = 3448
+untimely4_3_5 = 149
+
+timely4_3_6 = 3510
+untimely4_3_6 = 86
+
+timely4_3_7 = 3720
+untimely4_3_7 = 74
+
+timely5_2_4 = 2627
+untimely5_2_4 = 160
+
+timely5_2_5 = 2969
+untimely5_2_5 = 2
+
+#timely5_2_5 = 2913
+#untimely5_2_5 = 130
+
+timely5_2_6 = 3132
+untimely5_2_6 = 0
+
+timely5_2_7 = 3201
+untimely5_2_7 = 4
+
+timely4_2_4 = 3093
+untimely4_2_4 = 44
+
+#timely4_2_5 = 2826
+#untimely4_2_5 = 143
+
+timely4_2_5 = 3098
+untimely4_2_5 = 0
+
+timely4_2_6 = 3031
+untimely4_2_6 = 0
+
+timely4_2_7 = 3218
+untimely4_2_7 = 0
+
+timely6_2_4 = 2640
+untimely6_2_4 = 147
+
+'''
+timely6_2_5 = 2879
+untimely6_2_5 = 112
+'''
+
+timely6_2_5 = 3086
+untimely6_2_5 = 2
+
+timely6_2_6 = 2931
+untimely6_2_6 = 1
+
+timely6_2_7 = 3290
+untimely6_2_7 = 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
