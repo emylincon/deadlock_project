c5b42927fca70cbc153ac5f2da22b2bafb988c3e
Emeka Ugwuanyi <emylincon@gmail.com>
2020-03-10 15:50:00 +0000

    timings

--- 3algo/gui_user.py
+++ 3algo/gui_user.py
@@ -154,7 +154,7 @@ def get_tasks():
 def waiting_time_init():
     # t_time = {i: [round(r.uniform(0.4, 0.8), 3), round((tasks[i]['period']) / (tasks[i]['wcet']), 3)] for i in
     #           tasks}  # t_time = {'ti': [execution_time, latency], ..}
-    t_time = {i: [round(r.uniform(0.4, 0.8), 3), round(r.uniform(3,7), 3)] for i in
+    t_time = {i: [round(r.uniform(0.4, 0.8), 3), round(r.uniform(.1,.3), 3)] for i in
               tasks}
     return t_time
 
